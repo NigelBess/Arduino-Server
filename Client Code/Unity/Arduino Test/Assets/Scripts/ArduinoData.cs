44f839df2b1f0defc2e6836055378beeeb514a7a
nigelbess <nigelbess@gmail.com>
2019-09-09 16:32:51 -0700

    still working on unity client code

---
+++ Client Code/Unity/Arduino Test/Assets/Scripts/ArduinoData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ArduinoData
+{
+    public static string port;
+    public static string leftServoPin;
+    public static string rightServoPin;
+    public static string servoPowerPin;
+    public static string encoderInterruptPin;
+    public static string encoderSecondaryPin;
+    public static string irSensorPin;
+    public static string solenoidPin;
+}
---
+++ Client Code/Unity/Arduino Test/Assets/Scripts/UI/InputFields.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFields : MonoBehaviour
+{
+    [SerializeField] private InputField[] inputFields;
+    [SerializeField] private DataLogger dataLogger;
+    [SerializeField] private string fileName;
+
+    void Awake()
+    {
+        Load();
+    }
+    public void Load()
+    {
+        string[] fieldContents = dataLogger.ReadArray(fileName);
+        for (int i = 0; i < inputFields.Length; i++)
+        {
+            if (i<fieldContents.Length) inputFields[i].text = fieldContents[i];
+        }
+    }
+    public virtual void Save()
+    {
+        for (int i = 0; i < inputFields.Length; i++)
+        {
+            dataLogger.Save(fileName, inputFields[i].text, i > 0);
+        }
+    }
+}
